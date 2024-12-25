2c43fbbf049e8e116a9409ddf431e0964c3b65dc
anuruddham <charithtrice@gmail.com>
2024-12-26 00:06:36 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,19 @@
+# This is a simple Python program
+print("Hello, World!")
+# Taking user input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Outputting the information
+print("Hello, " + name + "!")
+print("You are " + age + " years old.")
+
+# Simple if-else statement
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+
+
